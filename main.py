53750a4861660d813cdb578594292c3c99d1a251
zihadev <34001897+zihadev@users.noreply.github.com>
2024-08-08 18:53:00 +0200

    fake temp added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+import random
 
 st.title("weather forecast for the next days".title())
 
@@ -14,3 +16,11 @@ options = ["Temperature", "Sky"]
 selected_option = st.selectbox("Select data to view:", options)
 day_label = "day" if days == 1 else "days"
 st.subheader(f"{selected_option} for the next {days} {day_label} for {place}")
+
+
+dates =['2022-25-10','2022-26-10','2022-27-10','2022-28-10','2022-29-10']
+temperatures = [10,12,15,5,7]
+temperatures = [i*random.randint(1,50)*days for i in temperatures]
+
+figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y":"Temperature(C)"})
+st.plotly_chart(figure)
