2b90147d4e1ec4d84124877655d058114ba0ea10
zihadev <34001897+zihadev@users.noreply.github.com>
2024-08-11 16:19:25 +0200

    data from API

---
+++ backend.py
@@ -0,0 +1,63 @@
+# coding=utf-8
+# we need: dates, how many days, temperatures, place
+from dotenv import load_dotenv
+import os
+import requests
+from datetime import datetime, timedelta
+
+load_dotenv()
+API_key = os.getenv('OWMAPIZH')
+
+if API_key:
+    print("API Key retrieved successfully.")
+else:
+    print("API Key not found.")
+
+
+def get_data(place, forecast_days, kind):
+    dates = []
+    temperatures = []
+    data, lon, lat = find_city(place)
+    weather_data = find_weather(lon, lat)
+    today = datetime.now().date()
+    end_date = today + timedelta(days=forecast_days)
+
+    for i in weather_data['list']:
+        date = datetime.strptime(i['dt_txt'], "%Y-%m-%d %H:%M:%S")
+        entry_date = date.date()
+        if today <= entry_date <= end_date:
+            dates.append(date)
+            temperatures.append(i['main']['temp'])
+    return dates, temperatures
+
+
+def find_city(city_name):
+    city_loc = (
+        f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}"
+        f"&appid={API_key}")
+    response = requests.get(city_loc)
+    if response.status_code == 200:
+        # Request was successful
+        data = response.json()  # Parse the JSON response
+        lon = data[0]['lon']
+        lat = data[0]['lat']
+        data = "CityOK"
+    else:
+        # Handle errors
+        data = f"Error: {response.status_code} - {response.text}"
+        lon = lat = ""
+    return data, lon, lat
+
+
+def find_weather(lon, lat):
+    weather = (f"https://api.openweathermap.org/data/2.5/"
+               f"forecast?lat={lat}&lon={lon}&appid={API_key}&units=metric")
+    response = requests.get(weather)
+    if response.status_code == 200:
+        # Request was successful
+        data = response.json()  # Parse the JSON response
+
+    else:
+        # Handle errors
+        data = f"Error: {response.status_code} - {response.text}"
+    return data
--- main.py
+++ main.py
@@ -2,13 +2,29 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import random
+from backend import get_data
+import os
 
-st.title("weather forecast for the next days".title())
 
-place = st.text_input("Enter Place:", value = "Zabrze")
+def clear_terminal():
+    # For Windows
+    if os.name == 'nt':
+        os.system('cls')
+    # For macOS and Linux (posix systems)
+    else:
+        os.system('clear')
 
-days = st.slider("Forecast Days:", min_value=1, max_value=5, help = "Select the number of forecasted days")
+
+# Example usage
+clear_terminal()
+print("started")
+
+st.markdown("## Weather Forecast for the Next Days".title())
+
+place = st.text_input("Enter Place:", value="Zabrze")
+
+days = st.slider("Forecast Days:", min_value=1, max_value=5,
+                 help="Select the number of forecasted days")
 
 # List of options
 options = ["Temperature", "Sky"]
@@ -17,10 +33,8 @@ selected_option = st.selectbox("Select data to view:", options)
 day_label = "day" if days == 1 else "days"
 st.subheader(f"{selected_option} for the next {days} {day_label} for {place}")
 
+dates, temperatures = get_data(place, days, selected_option)
 
-dates =['2022-25-10','2022-26-10','2022-27-10','2022-28-10','2022-29-10']
-temperatures = [10,12,15,5,7]
-temperatures = [i*random.randint(1,50)*days for i in temperatures]
-
-figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y":"Temperature(C)"})
-st.plotly_chart(figure)
+figure = px.line(x=dates, y=temperatures,
+                 labels={"x": "Date", "y": "Temperature(C)"})
+st.plotly_chart(figure, on_select="rerun")
